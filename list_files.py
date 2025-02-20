ea6f1e0a8d1f1eac9e160f64da45f8405f420730
CursedGreanbeen <101968163+CursedGreanbeen@users.noreply.github.com>
2025-02-20 22:41:05 +0500

    Update list_files.py

--- list_files.py
+++ list_files.py
@@ -12,6 +12,8 @@ def list_names(path):
         if ('.pdbqt' in file) and ('clean' not in file) and ('out' not in file):
             ligands.append(file[:-6])
 
+    return proteins, ligands
+
 
 # proteins = [prot[:-12] for prot in dir_list if '_clean.pdbqt' in prot]
 # ligands = [lig[:-6] for lig in dir_list if ('.pdbqt' in lig and 'clean' not in lig)]

3234801d5a218d657385b3782e5d9a49d76b4d66
Adelina <101968163+CursedGreanbeen@users.noreply.github.com>
2026-03-26 16:03:49 +0500

    Add files via upload

--- AD_Vina_GUI.py
+++ AD_Vina_GUI.py
@@ -1,34 +1,41 @@
 import tkinter as tk
+from tkinter import ttk
 from real_auto_dock import auto_autodock
-from list_files import list_names
+from list_files import list_names, list_prots
 
 
 def main():
-    proteins = []
-    ligands = []
+    proteins, ligands = [], []
+    dock_type = 'simple'
 
     def names():
         nonlocal proteins, ligands
         proteins, ligands = list_names(proteins, ligands)
-        list_of_prots = tk.Label(text=proteins)
-        list_of_ligs = tk.Label(text=ligands)
+        list_of_prots = ttk.Label(text=proteins)
+        list_of_ligs = ttk.Label(text=ligands)
         list_of_prots.pack()
         list_of_ligs.pack()
 
+    def prots():
+        nonlocal proteins
+        proteins = list_prots()
+        list_of_prots = ttk.Label(text=proteins)
+        list_of_prots.pack()
+
     def dock():
-        nonlocal proteins, ligands
+        nonlocal proteins, ligands, dock_type
         if proteins and ligands:
-            auto_autodock(proteins, ligands)
+            auto_autodock(proteins, ligands, dock_type)
             proteins, ligands = [], []
 
     window = tk.Tk()
     greet = tk.Label(text='Enter proteins and ligands')
     greet.pack()
 
-    prots_label = tk.Label(text='proteins')
-    prots_entry = tk.Entry()
-    ligs_label = tk.Label(text='ligands')
-    ligs_entry = tk.Entry()
+    prots_label = ttk.Label(text='proteins')
+    prots_entry = ttk.Entry()
+    ligs_label = ttk.Label(text='ligands')
+    ligs_entry = ttk.Entry()
 
     prots_label.pack()
     prots_entry.pack()
@@ -46,21 +53,22 @@ def main():
         prots_entry.delete(0, tk.END)
         ligs_entry.delete(0, tk.END)
 
-        # Для проверки вы можете вывести текущие значения в консоль
         print("Proteins:", proteins)
         print("Ligands:", ligands)
 
-    # Кнопка для добавления введенных данных
-    submit_button = tk.Button(text='Submit', command=entries)
+    submit_button = ttk.Button(text='Submit', command=entries)
     submit_button.pack()
 
-    auto_list = tk.Label(text='or gather names automatically:')
+    auto_list = ttk.Label(text='or gather names automatically:')
     auto_list.pack()
 
-    butt_names = tk.Button(text='Make a list of names', command=names)
+    butt_names = ttk.Button(text='Make a list of all names', command=names)
     butt_names.pack()
 
-    butt_dock = tk.Button(text='Dock!', command=dock)
+    butt_names = ttk.Button(text='Make a list of all prots', command=prots)
+    butt_names.pack()
+
+    butt_dock = ttk.Button(text='Dock!', command=dock)
     butt_dock.pack()
 
     window.mainloop()
--- real_auto_dock.py
+++ real_auto_dock.py
@@ -11,63 +11,79 @@ def replace_line(file_name, line_num, text):
     out.close()
 
 
-def auto_autodock(proteins, ligands):
+def auto_autodock(proteins, ligands, dock_type):
     path = r'cd "C:\Users\Nadezhda\OneDrive\Desktop\Dock"'
     os.system(path)
 
     with open('total_nrg.txt', 'w+', encoding='utf-8') as res:
         for prot in proteins:
-            # with open(f'{prot}_blind_config.txt', 'r') as config:
-            with open(f'{prot}_config.txt', 'r', encoding='utf-8') as config:
-                data = config.readlines()
 
-            for lig in ligands:
-                data[1] = f'ligand = {lig}.pdbqt\n'
-                # with open(f'{prot}_blind_config.txt', 'w') as out:
-                with open(f'{prot}_config.txt', 'w', encoding='utf-8') as out:
-                    out.writelines(data)
+            if dock_type != 'blind':
+                with open(f'{prot}_config.txt', 'r', encoding='utf-8') as config:
+                    data = config.readlines()
 
-                # cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe"' \
-                #       f' --receptor {prot}_clean.pdbqt' \
-                #       f' --ligand {lig}.pdbqt' \
-                #       f' --config {prot}_config.txt' \
-                #       f' --out {prot}_{lig}_out.pdbqt'\
-                #       f' --log {prot}_{lig}_log.txt'
-                #
-                # os.system(cmd)
-                # lines = 25
+                for lig in ligands:
+                    data[1] = f'ligand = {lig}.pdbqt\n'
+                    with open(f'{prot}_config.txt', 'w', encoding='utf-8') as out:
+                        out.writelines(data)
 
-                # double docking
-                cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina_1_2_3.exe"' \
-                      f' --receptor {prot}_clean.pdbqt' \
-                      f' --ligand {lig}.pdbqt {lig}.pdbqt' \
-                      f' --config {prot}_config.txt' \
-                      f' --out {prot}_double_{lig}_out.pdbqt'\
+                    if dock_type == 'simple':
+                        cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe"' \
+                              f' --receptor {prot}_clean.pdbqt' \
+                              f' --ligand {lig}.pdbqt' \
+                              f' --config {prot}_config.txt' \
+                              f' --out {prot}_{lig}_out.pdbqt'\
+                              f' --log {prot}_{lig}_log.txt'
 
-                result = subprocess.run(
-                    cmd,
-                    capture_output=True,
-                    text=True,
-                    encoding='utf-8',
-                    errors='replace',
-                    shell=True
-                )
+                        os.system(cmd)
+                        lines = 25
 
-                with open(f'{prot}_{lig}_log.txt', 'w', encoding='utf-8') as f:
-                    f.write(result.stdout)
+                    elif dock_type == 'double':
+                        cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina_1_2_3.exe"' \
+                              f' --receptor {prot}_clean.pdbqt' \
+                              f' --ligand {lig}.pdbqt {lig}.pdbqt' \
+                              f' --config {prot}_config.txt' \
+                              f' --out {prot}_double_{lig}_out.pdbqt'
 
-                lines = 40
+                        result = subprocess.run(
+                            cmd,
+                            capture_output=True,
+                            text=True,
+                            encoding='utf-8',
+                            errors='replace',
+                            shell=True
+                        )
 
-                # blind docking
-                # cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe"' \
-                #       f' --receptor {prot}_clean.pdbqt' \
-                #       f' --ligand {lig}.pdbqt' \
-                #       f' --config {prot}_blind_config.txt' \
-                #       f' --out {prot}_{lig}_blind_out.pdbqt' \
-                #       f' --log {prot}_{lig}_log.txt'
-                #
-                # os.system(cmd)
-                # lines = 25
+                        with open(f'{prot}_{lig}_log.txt', 'w', encoding='utf-8') as f:
+                            f.write(result.stdout)
+                        lines = 40
+
+                    with open(f'{prot}_{lig}_log.txt', encoding='utf-8') as log:
+                        for i in range(lines):
+                            line = log.readline()
+                            if 'WARNING' in line:
+                                log.readline()
+                        res.write(f'{prot} {lig} {log.readline()}')
+                res.write(f'{'=' * 60}\n')
+
+            elif dock_type == 'blind':
+                with open(f'{prot}_blind_config.txt', 'r') as config:
+                    data = config.readlines()
+
+                    for lig in ligands:
+                        data[1] = f'ligand = {lig}.pdbqt\n'
+                        with open(f'{prot}_blind_config.txt', 'w', encoding='utf-8') as out:
+                            out.writelines(data)
+
+                    cmd = r'"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe"' \
+                          f' --receptor {prot}_clean.pdbqt' \
+                          f' --ligand {lig}.pdbqt' \
+                          f' --config {prot}_blind_config.txt' \
+                          f' --out {prot}_{lig}_blind_out.pdbqt' \
+                          f' --log {prot}_{lig}_log.txt'
+
+                    os.system(cmd)
+                    lines = 25
 
                 with open(f'{prot}_{lig}_log.txt', encoding='utf-8') as log:
                     for i in range(lines):
